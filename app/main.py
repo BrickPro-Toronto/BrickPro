6340a86f91e51d3d604bb876fa1a148dffc06d6f
BrickPro-Toronto <potatostornado@gmail.com>
2025-06-08 23:27:52 -0400

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,11 +1,6 @@
-
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "✅ BrickPro API is running."
-
-if __name__ == '__main__':
-    app.run()
+    return "BrickPro API is running."
